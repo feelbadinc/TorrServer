7d2e7b3dcd1d10f8783bc97bd9c6165add24dac9
feelbadinc <132283466+feelbadinc@users.noreply.github.com>
2026-05-18 17:35:10 +0300

    chore(web): replace manual vite aliases with vite-tsconfig-paths

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,21 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), nodePolyfills({ include: ['buffer', 'fs'] })],
-  resolve: {
-    alias: {
-      'style': path.resolve(__dirname, 'src/style'),
-      'components': path.resolve(__dirname, 'src/components'),
-      'utils': path.resolve(__dirname, 'src/utils'),
-      'icons': path.resolve(__dirname, 'src/icons'),
-      'locales': path.resolve(__dirname, 'src/locales'),
-      'torrentStates': path.resolve(__dirname, 'src/torrentStates.js'),
-      'i18n': path.resolve(__dirname, 'src/i18n.js'),
-    },
-  },
+  plugins: [react(), nodePolyfills({ include: ['buffer', 'fs'] }), tsconfigPaths()],
   server: {
     open: true,
     proxy: {
